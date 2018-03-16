6e2b4ff9898e4f9ad20aabb9e85c51d24bba6da6
asdzxc55888 <asdzxc55888@gmail.com>
2018-03-16 09:47:50 +0800

    8

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -8,7 +8,12 @@ bool Guess(int number) {
 		Random r;
 		target = r.Next() % 100 + 1;
 	}
-	if (number == target)return true;
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else std::cout << "Wrong" << std::endl;
 	return false;
 }
 int main(array<System::String ^> ^args)
